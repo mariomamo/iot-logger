c14d4d01bd650eef4be173fcd49a77b67349d4ea
mariomamo <mariooffertucci@live.it>
2021-07-03 17:27:46 +0200

    Some refactor

--- iot-sensors/sensors/AirConditionerSensor.py
+++ iot-sensors/sensors/AirConditionerSensor.py
@@ -14,32 +14,53 @@ class AirConditionerSensor(Sensor):
         super(AirConditionerSensor, self).__init__(sensorType, chatId, name, image, send_topic_name)
         self.__status = 20
 
+    @property
+    def status(self):
+        return self.__status
+
+    @status.setter
+    def status(self, status):
+        self.__status = status
+
     def message_handler(self, body):
-        message = "I'm sorry, i'm not understand your command"
-        payload = PayloadDTO(self.getHour(), "text", message)
         userId = body['userId']
+        defaultMessage = (lambda: "I'm sorry, I'm not understand your command", )
 
-        if body['payload']['message'].lower() == 'set 20°':
-            message = "Temperature setted to 20°"
-            self.__status = 20
-        elif body['payload']['message'].lower() == 'set 22°':
-            message = "Temperature setted to 22°"
-            self.__status = 22
-        elif body['payload']['message'].lower() == 'set 24°':
-            message = "Temperature setted to 24°"
-            self.__status = 24
-        elif body['payload']['message'].lower() == 'set 26°':
-            message = "Temperature setted to 26°"
-            self.__status = 26
-        elif body['payload']['message'].lower() == 'set 28°':
-            message = "Temperature setted to 28°"
-            self.__status = 28
-        elif body['payload']['message'].lower() == 'set 30°':
-            message = "Temperature setted to 30°"
-            self.__status = 30
-        elif body['payload']['message'].lower() == 'status':
-            message = f"The temperature is {self.__status}°"
+        message = {
+            'set 20°': (self.__set_temperature_and_return_message, 20),
+            'set 22°': (self.__set_temperature_and_return_message, 22),
+            'set 24°': (self.__set_temperature_and_return_message, 24),
+            'set 26°': (self.__set_temperature_and_return_message, 26),
+            'set 28°': (self.__set_temperature_and_return_message, 28),
+            'set 30°': (self.__set_temperature_and_return_message, 30),
+            'status': (lambda: f'The temperature is {self.__status}°', )
+        }.get(body['payload']['message'], defaultMessage)
 
-        payload.message = message
+        params = message[1:]
+        message = message[0](*params)
 
+        payload = PayloadDTO(self.getHour(), "text", message)
         return ResponseMessageDTO(self.sensorType, self.chatId, self.name, self.image, payload, userId=userId)
+
+    def __set_temperature_and_return_message(self, temp):
+        self.__status = temp
+        return f'Temperature setted to {self.__status}°'
+
+
+if __name__ == '__main__':
+    airConditionerSensor = AirConditionerSensor('air-conditioner', 'TEST_CHAT_ID', 'NAME', 'IMAGE', 'SENSOR_TOPIC_NAME')
+    print(airConditionerSensor.message_handler({'userId': 'USER_ID', 'payload': {'message': 'set 20°'}}))
+    print(f'Status: {airConditionerSensor.status}')
+    print(airConditionerSensor.message_handler({'userId': 'USER_ID', 'payload': {'message': 'set 22°'}}))
+    print(f'Status: {airConditionerSensor.status}')
+    print(airConditionerSensor.message_handler({'userId': 'USER_ID', 'payload': {'message': 'set 24°'}}))
+    print(f'Status: {airConditionerSensor.status}')
+    print(airConditionerSensor.message_handler({'userId': 'USER_ID', 'payload': {'message': 'set 26°'}}))
+    print(f'Status: {airConditionerSensor.status}')
+    print(airConditionerSensor.message_handler({'userId': 'USER_ID', 'payload': {'message': 'set 28°'}}))
+    print(f'Status: {airConditionerSensor.status}')
+    print(airConditionerSensor.message_handler({'userId': 'USER_ID', 'payload': {'message': 'set 30°'}}))
+    print(f'Status: {airConditionerSensor.status}')
+    print(airConditionerSensor.message_handler({'userId': 'USER_ID', 'payload': {'message': 'status'}}))
+    print(f'Status: {airConditionerSensor.status}')
+    print(airConditionerSensor.message_handler({'userId': 'USER_ID', 'payload': {'message': 'wrong message'}}))
